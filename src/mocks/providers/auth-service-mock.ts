e1a7575836a1ac5cff3c7cf1cda04a3e56c26c5d
Sandro Zbinden <sandrozbinden@gmail.com>
2017-06-20 16:41:26 +0200

    refactor: use imsbackendmock to initialize authservicemock

--- src/mocks/providers/auth-service-mock.ts
+++ src/mocks/providers/auth-service-mock.ts
@@ -1,24 +1,16 @@
-import { Credential } from '../../models/credential';
-import { Observable } from 'rxjs/Observable';
-import { Info } from '../../models/info';
+import { ImsBackendMock } from './../ims-backend-mock';
+import { SettingService } from './../../providers/setting-service';
+import { ImsService } from './../../providers/ims-service';
+import { Http } from '@angular/http';
+import { AuthService } from './../../providers/auth-service';
+import { Injectable } from '@angular/core';
 
-export class AuthServiceMock {
+@Injectable()
+export class AuthServiceMock extends AuthService {
 
-  infoMock: Info = { version: '9000'};
-  currentCredential: Credential;
-
-  login(credentials): Observable<Info> {
-    this.setCurrentCredential = credentials;
-    return Observable.of(this.infoMock);
-  }
-
-  logout() {
-    this.currentCredential = null;
-  }
-
-  setCurrentCredential(info: Info, credentials: Credential): Info {
-    this.currentCredential = credentials;
-    return info;
-  }
+  constructor(public http: Http, public imsService: ImsService, public settingService: SettingService, public imsBackendMock: ImsBackendMock) {
+    super(http, imsService, settingService);
+    this.currentCredential = imsBackendMock.credential;
+    this.setArchive(imsBackendMock.policeFilter);
 
 }
