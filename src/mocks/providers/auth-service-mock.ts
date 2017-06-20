69258696df3974d48d74402b8aeb8bdd418364e6
Sandro Zbinden <sandrozbinden@gmail.com>
2017-06-20 17:09:34 +0200

    chore: add missing closing bracet

--- src/mocks/providers/auth-service-mock.ts
+++ src/mocks/providers/auth-service-mock.ts
@@ -12,5 +12,6 @@ export class AuthServiceMock extends AuthService {
     super(http, imsService, settingService);
     this.currentCredential = imsBackendMock.credential;
     this.setArchive(imsBackendMock.policeFilter);
+  }
 
 }
